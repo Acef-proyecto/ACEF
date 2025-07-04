2ae9558179198c64db39ffc4120569a13477f2b0
Marcela <leslyemarcelab@gmail.com>
2025-07-03 21:21:30 -0500

    manual tecnico

--- backend/config/jwt.js
+++
@@ -1,6 +0,0 @@
-// backend/config/jwt.js
-require('dotenv').config();
-module.exports = {
-  secret: process.env.JWT_SECRET,
-  expiresIn: process.env.JWT_EXPIRES_IN
-};
--- backend/routes/alerta.routes.js
+++
@@ -1,7 +0,0 @@
-// backend/routes/alerta.routes.js
-const router = require('express').Router();
-const alertaCtrl = require('../controllers/alerta.controller');
-
-router.post('/trimestre', alertaCtrl.enviarPorTrimestre);
-
-module.exports = router;
